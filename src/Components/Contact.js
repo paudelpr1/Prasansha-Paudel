312fdf5a334879dfc654ea2469706a158cf014d2
paudelpr1 <prasanshapaudel111@gmail.com>
2023-08-31 11:57:53 -0500

    Remove warnings

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Button from '@mui/material/Button';
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 
 function Contact(){
